bb98cf50ac29018fb19171d1686ade3a1cd95aea
SangYoonLee <sylagape1231@gmail.com>
2021-11-15 00:50:52 +0900

    Update All
    11/15(일) 4일차
    
    - 예매 기능 - 4가지 정보 값 선택해서 변수에 저장하는 기능 구현

--- js/booking.js
+++ js/booking.js
@@ -1,68 +1,95 @@
-const A1 = document.getElementById("A1");
-const A2 = document.getElementById("A2");
-const A3 = document.getElementById("A3");
-const A4 = document.getElementById("A4");
-const A5 = document.getElementById("A5");
-const A6 = document.getElementById("A6");
-const A7 = document.getElementById("A7");
-const A8 = document.getElementById("A8");
-const A9 = document.getElementById("A9");
-const A10 = document.getElementById("A10");
+const seatTable = document.getElementById("seat-chart-div");
+const movieTable = document.getElementById("movie-select-div");
 
-const B1 = document.getElementById("B1");
-const B2 = document.getElementById("B2");
-const B3 = document.getElementById("B3");
-const B4 = document.getElementById("B4");
-const B5 = document.getElementById("B5");
-const B6 = document.getElementById("B6");
-const B7 = document.getElementById("B7");
-const B8 = document.getElementById("B8");
-const B9 = document.getElementById("B9");
-const B10 = document.getElementById("B10");
+// 영화 선택 영역
+let movie = null;
+let movie_id = null;
+let movie_color = false;
 
-const C1 = document.getElementById("C1");
-const C2 = document.getElementById("C2");
-const C3 = document.getElementById("C3");
-const C4 = document.getElementById("C4");
-const C5 = document.getElementById("C5");
-const C6 = document.getElementById("C6");
-const C7 = document.getElementById("C7");
-const C8 = document.getElementById("C8");
-const C9 = document.getElementById("C9");
-const C10 = document.getElementById("C10");
-
-const D1 = document.getElementById("D1");
-const D2 = document.getElementById("D2");
-const D3 = document.getElementById("D3");
-const D4 = document.getElementById("D4");
-const D5 = document.getElementById("D5");
-const D6 = document.getElementById("D6");
-const D7 = document.getElementById("D7");
-const D8 = document.getElementById("D8");
-const D9 = document.getElementById("D9");
-const D10 = document.getElementById("D10");
-
-const E1 = document.getElementById("E1");
-const E2 = document.getElementById("E2");
-const E3 = document.getElementById("E3");
-const E4 = document.getElementById("E4");
-const E5 = document.getElementById("E5");
-const E6 = document.getElementById("E6");
-const E7 = document.getElementById("E7");
-const E8 = document.getElementById("E8");
-const E9 = document.getElementById("E9");
-const E10 = document.getElementById("E10");
-
-const seatTable = document.getElementById("seat-chart");
-
-const movie_name;
-const date;
-const seat;
-
-function handleClick(event) {
-    seat = seatTable;
+function handleMovieClick(event) {
+  if (event.target.id == "movie-select-div") {
+  } else if (event.target.id == "movies") {
+  } else if (movie_color == false) {
+    movie = event.target;
+    movie.classList.add("click");
+    movie_id = movie.id;
+    movie_color = true;
+  } else {
+    if (event.target.id == movie_id) {
+      movie_id = null;
+      movie.classList.toggle("click");
+      movie_color = false;
+    } else {
+      event.target.classList.add("click");
+      movie.classList.remove("click");
+      movie = event.target;
+      movie_id = movie.id;
+    }
+  }
 }
 
-//A1.addEventListener("click", handleClick);
+movieTable.addEventListener("click", handleMovieClick);
+
+// 날짜, 시간 선택 영역
+const date = document.getElementById("date");
+const time = document.getElementById("time");
+
+let selectedDate = null;
+let selectedTime = null;
+
+function changeDate() {
+  selectedDate = date.options[date.selectedIndex].text;
+}
+
+date.addEventListener("change", changeDate);
+
+function changeTime() {
+  selectedTime = time.options[time.selectedIndex].text;
+}
+
+time.addEventListener("change", changeTime);
+
+// 좌석 선택 영역
+let seat = null;
+let seat_id = null;
+let seat_color = false;
+
+function handleClick(event) {
+  if (event.target.id == "table-seat") {
+  } else if (event.target.id == "seat-chart-div") {
+  } else if (seat_color == false) {
+    seat = event.target;
+    seat.classList.add("click");
+    seat_id = seat.id;
+    seat_color = true;
+  } else {
+    if (event.target.id == seat_id) {
+      seat_id = null;
+      seat.classList.toggle("click");
+      seat_color = false;
+    } else {
+      event.target.classList.add("click");
+      seat.classList.remove("click");
+      seat = event.target;
+      seat_id = seat.id;
+    }
+  }
+}
 
 seatTable.addEventListener("click", handleClick);
+
+// 예매 정보 모두 선택 완료시 값 저장 및 초기화
+const button = document.getElementById("submit");
+
+function onBtnClick() {
+  console.log(movie.innerText);
+  console.log(selectedDate);
+  console.log(selectedTime);
+  console.log(seat_id);
+  // 시간, 날짜 모두 겹치는지 확인해서 localStorage에 추가
+  // localStorage 저장시, 예매내역 HTMl태그 JS에서 생성, 추가
+  // (이 때, 예매 내역 '삭제' 기능 태그,
+  // 예매 내역 '확인' 기능도 만들어야함)
+}
+
+button.addEventListener("click", onBtnClick);
